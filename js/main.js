d757f071279e65e6736361acfb33c95ab52dae9a
Andrewbui389 <andrewbui389@gmail.com>
2022-06-27 22:35:53 -0700

    Beginning BattleShip Logic Planning

--- js/main.js
+++ js/main.js
@@ -1,1 +1,73 @@
+const shipsObj = {
+    carrier: 5,
+    battleship: 4,
+    cruiser: 3,
+    submarine: 3,
+    destroyer: 2
+}
+
+
+
+
 const startBtn = document.querySelector('.startGame')
+
+
+let testBoard 
+
+function makeBoard() { 
+    const board = []
+    let count = 0
+    while(count<11){
+        let row = makeRows()
+        board.push(row)
+        count++
+    }
+    
+    testBoard = board
+
+}
+
+function makeRows() {
+    let row = []
+    let count = 0
+    while(count<11){
+        row.push(null)
+        count++
+    }
+    return row
+}
+
+function currentShip() {
+    for(i of shipsObj){
+        console.log(i)
+    }
+}
+
+function shipPlacementVertical(length) { 
+    let shipLength = length
+    let count = 0
+    let locationY = 0
+    while(count<shipLength){
+    if(testBoard[count][locationY])return('spot taken')
+    count++
+    }
+    count = 0
+    while(count < shipLength){
+        testBoard[count][locationY] = 'x'
+        count++ 
+    }
+}
+
+function fire() {
+    let x = 0
+    let y = 0
+
+    if(testBoard[y][x]){
+        testBoard[y][x] = 'h'
+    }else{
+        testBoard[y][x] = 'm'
+    }
+}
+
+makeBoard()
+testBoard[0][0] = 'x'
--- pseudocode.md
+++ pseudocode.md
@@ -1,1 +1,2 @@
-h1Steps 
+# Steps and Logical Planning 
+When the start button is clicked a table containing the grid for the player to place their battleships will appear
