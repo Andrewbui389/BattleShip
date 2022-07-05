188bf7bc702662e75f29c3e773cd44519aaecfba
Andrewbui389 <andrewbui389@gmail.com>
2022-07-05 09:59:07 -0700

    Cleaned code

--- js/testing.js
+++ js/testing.js
@@ -6,6 +6,10 @@ const shipsId = [
                 {l:2, s:'p',sank:false,ship:'Patrol Boat',msgRead:false}
                 ]
 
+const wavesSound = new Audio('./Sounds/waves.mp3')
+
+const cannonFire = new Audio('./Sounds/cannonEffect.mp3')
+
 let player 
 
 let computer 
@@ -16,254 +20,248 @@ let y
 
 let rotation
 
-let winner 
+let winner;
 
-let it
-
-const wavesSound = new Audio('./Sounds/waves.mp3')
-
-const cannonFire = new Audio('./Sounds/cannonEffect.mp3')
-
-const tableELHum = document.querySelector("#player")
-
-const cellDataHum = [...tableELHum.querySelectorAll('td')]
-
-const tableELComp = document.querySelector("#computer")
-
-const cellDataComp = [...tableELComp.querySelectorAll('td')]
-
-const message = document.querySelector("h1")
-
-const rotateButton = document.querySelector('button')
+let it;
 
 
-tableELHum.addEventListener('click', handleData)
+const tableELHum = document.querySelector("#player");
 
-tableELHum.addEventListener('mouseover', highLight)
+const cellDataHum = [...tableELHum.querySelectorAll('td')];
 
-tableELHum.addEventListener('mouseout', unHighLight)
+const tableELComp = document.querySelector("#computer");
 
-tableELComp.addEventListener('click', handleData)
+const cellDataComp = [...tableELComp.querySelectorAll('td')];
 
-rotateButton.addEventListener('click', rotateBtn)
+const message = document.querySelector("h1");
+
+const rotateButton = document.querySelector('button');
+
+
+tableELHum.addEventListener('click', handleData);
+
+tableELHum.addEventListener('mouseover', highLight);
+
+tableELHum.addEventListener('mouseout', unHighLight);
+
+tableELComp.addEventListener('click', handleData);
+
+rotateButton.addEventListener('click', rotateBtn);
 
 
 init()
 
 function init() {
     
-player = makeDataBoard()
+player = makeDataBoard();
 
-computer = makeDataBoard()
+computer = makeDataBoard();
 
 it = {
         i: 0, 
         shipIdx:0,
         shipMsg: 0
-        }
+        };
 
-x = null 
+x = null;
 
-y = null
+y = null;
 
-rotation = 1 
+rotation = 1; 
 
-winner = null
+winner = null;
 
-tableELHum.style.pointerEvents = 'auto'
+tableELHum.style.pointerEvents = 'auto';
 
-tableELComp.style.pointerEvents = 'none'
+tableELComp.style.pointerEvents = 'none';
 
-renderMessage()
+renderMessage();
 
-CompPlace()
+CompPlace();
 
-resetBoard()
+resetBoard();
 
-}
+};
 
 function resetBoard() {
     for(i of cellDataHum){
-        i.style.backgroundColor = '#5199CC'
-        i.style.color = '#5199CC'
-    }
+        i.style.backgroundColor = '#5199CC';
+        i.style.color = '#5199CC';
+    };
     for(i of cellDataComp){
-        i.style.backgroundColor = '#5199CC'
-        i.style.color = '#5199CC'
-    }
-}
+        i.style.backgroundColor = '#5199CC';
+        i.style.color = '#5199CC';
+    };
+};
 
 function makeDataBoard(){
     let board = []
     while(board.length<10){
         board.push([null,null,null,null,null,null,null,null,null,null]);
-    }
+    };
     let data = {
         b: board,
         c: {} 
-    }
-    return data
-}
+    };
+    return data;
+};
 
 function handleData(evt){
-    if(evt.target.tagName !== 'TD')return
-    getCoors(evt.target.textContent)
-    it.i = 0
-    if(guard())return
+    if(evt.target.tagName !== 'TD')return;
+    getCoors(evt.target.textContent);
+    it.i = 0;
+    if(guard())return;
     else{
-    it.shipIdx < 5 ? addData() : fire()
-    it.shipIdx < 4 ? null : mouseActions()
-    render()
-    it.shipIdx++
-    }
-}
+    it.shipIdx < 5 ? addData() : fire();
+    it.shipIdx < 4 ? null : mouseActions();
+    render();
+    it.shipIdx++;
+    };
+};
 
 function addData() {
-    let X = x 
-    let Y = y
+    let X = x;
+    let Y = y;
     while(it.i < shipsId[it.shipIdx].l){
-        player.c[`${Y}${X}`] = true
-        player.b[Y][X] = shipsId[it.shipIdx].s
-        it.i++
-        rotation === 1 ? X++ : Y++
-    }
-    it.i = 0
-}
+        player.c[`${Y}${X}`] = true;
+        player.b[Y][X] = shipsId[it.shipIdx].s;
+        it.i++;
+        rotation === 1 ? X++ : Y++;
+    };
+    it.i = 0;
+};
 
 function mouseActions(){ 
         tableELHum.style.pointerEvents = 'none';
         tableELComp.style.pointerEvents = 'auto';
-        rotateButton.style.visibility = 'hidden'
-}
+        rotateButton.style.visibility = 'hidden';
+};
 
 function getCoors(int){
-    let newArr = int.split('')
-    y = parseInt(newArr[0])
-    x = parseInt(newArr[1])
-}
+    let newArr = int.split('');
+    y = parseInt(newArr[0]);
+    x = parseInt(newArr[1]);
+};
 
 function render(){
-    renderHumanBoard()
-    renderCompBoard()
-    renderMessage()
-    renderSound()
-}
+    renderHumanBoard();
+    renderCompBoard();
+    renderMessage();
+    renderSound();
+};
 
 
 function renderSound(){
-    wavesSound.volume = .5
-    wavesSound.play()
+    wavesSound.volume = .5;
+    wavesSound.play();
     if(it.shipIdx>4){
-        cannonFire.duration = 1
-        cannonFire.play()
-    }
-    
-    
-}
+        cannonFire.duration = 1;
+        cannonFire.play();
+    };
+};
 
 function renderHumanBoard(){
     for(i of cellDataHum){
         if(player.c[i.textContent]){
-            i.style.backgroundColor = '#4F5F63'
-        }
+            i.style.backgroundColor = '#4F5F63';
+        };
         if(player.c[i.textContent] === 'H'){
-            i.style.backgroundColor = 'red'
-        }
+            i.style.backgroundColor = 'red';
+        };
         if(player.c[i.textContent] === 'M'){
-            i.style.backgroundColor = 'green'
-        }
-    }
-}
+            i.style.backgroundColor = 'green';
+        };
+    };
+};
 
 function renderCompBoard(){
     for(i of cellDataComp){
         if(computer.c[i.textContent] === 'H'){
-            i.style.backgroundColor = 'red'
-        }
+            i.style.backgroundColor = 'red';
+        };
         if(computer.c[i.textContent] === 'M'){
-            i.style.backgroundColor = 'green'
-        }
-    }
-    
-}
+            i.style.backgroundColor = 'green';
+        };
+    };
+};
 
 function renderMessage(){
     if(it.shipIdx < 4){
-        message.textContent = `Admiral Please Place ${shipsId[it.shipMsg].ship}`
-        it.shipMsg++
-    } 
+        message.textContent = `Admiral Please Place ${shipsId[it.shipMsg].ship}`;
+        it.shipMsg++;
+    };
     if(it.shipIdx > 4){
         if(computer.c[`${y}${x}`] === 'H'){
-            message.textContent = 'Hit'
+            message.textContent = 'Hit';
         } else if(computer.c[`${y}${x}`] !== 'H'){
-            message.textContent = 'Miss'
-        }
-    }
-}
+            message.textContent = 'Miss';
+        };
+    };
+};
 
 function guard(){
-    if(i.shipIdx > 4)return 
-    let X = x
-    let Y = y
-    if(shipsId[it.shipIdx] === undefined)return
+    if(i.shipIdx > 4)return; 
+    let X = x;
+    let Y = y;
+    if(shipsId[it.shipIdx] === undefined)return;
     while(it.i < shipsId[it.shipIdx].l){
-        if(Y > 9)return true
-        if(player.b[Y][X] || player.b[Y][X] === undefined)return true
-        rotation === 1 ? X++ : Y++
-        it.i++  
-    }
-    it.i = 0
-}
+        if(Y > 9)return true;
+        if(player.b[Y][X] || player.b[Y][X] === undefined)return true;
+        rotation === 1 ? X++ : Y++;
+        it.i++;  
+    };
+    it.i = 0;
+};
 
 
 //Functions for Highlighting and Unhighlighting ships
 
 function highLight(evt){
-    it.i = 0
-    if(evt.target.tagName !== 'TD')return
-    getCoors(evt.target.textContent)
-    if(guard())return
-    let X = x
-    let Y = y
+    it.i = 0;
+    if(evt.target.tagName !== 'TD')return;
+    getCoors(evt.target.textContent);
+    if(guard())return;
+    let X = x;
+    let Y = y;
     while(it.i < shipsId[it.shipIdx].l){
         for(i of cellDataHum){
-            if(i.textContent === `${Y}${X}` && i.style.backgroundColor !== '#4F5F63'){i.style.backgroundColor = 'lightgray'}
-        }
-        rotation === 1 ? X++ : Y++
-        it.i++
-    }
-    it.i = 0
-}
+            if(i.textContent === `${Y}${X}` && i.style.backgroundColor !== '#4F5F63'){i.style.backgroundColor = 'lightgray'};
+        };
+        rotation === 1 ? X++ : Y++;
+        it.i++;
+    };
+    it.i = 0;
+};
 
 function unHighLight(evt){
-    if(evt.target.tagName !== 'TD')return
-    getCoors(evt.target.textContent)
-    if(guard())return
+    if(evt.target.tagName !== 'TD')return;
+    getCoors(evt.target.textContent);
+    if(guard())return;
     for(i of cellDataHum){
         if(i.style.backgroundColor === 'lightgray'){
-            i.style.backgroundColor = '#5199CC'
-        }
-    }
-}
+            i.style.backgroundColor = '#5199CC';
+        };
+    };
+};
 
 function rotateBtn() {
     rotation === 1 ? rotation =  0 : rotation = 1;
-}
+};
 
 function checkShips() { 
     
-}
+};
 
 function fire(){
-    if(computer.c[`${y}${x}`] === 'M')return
+    if(computer.c[`${y}${x}`] === 'M')return;
     if(computer.b[y][x]){
-     (computer.c[`${y}${x}`] = 'H')
-     return true
+     (computer.c[`${y}${x}`] = 'H');
+     return true;
     }else{
-     (computer.c[`${y}${x}`] = 'M')
-    }
-    console.log('fire')
-}
+     (computer.c[`${y}${x}`] = 'M');
+    };
+    console.log('fire');
+};
 
 
 
@@ -271,40 +269,36 @@ function fire(){
 
 
 function CompPlace() { 
-    let count = 0
+    let count = 0;
     while(count < 5){
-        randomLocation(count)
-        count++
-    }
-}
-
+        randomLocation(count);
+        count++;
+    };
+};
 
 function randomLocation(int) {
-    let count = 0
-    let X = randomCoor(int)
-    let Y = randomCoor(int)
-    let randomRotation = Math.floor(Math.random() * 2)
+    let count = 0;
+    let X = randomCoor(int);
+    let Y = randomCoor(int);
+    let randomRotation = Math.floor(Math.random() * 2);
     while(count<shipsId[int].l){
-        computer.b[Y][X] = shipsId[int].s
-        computer.c[`${Y}${X}`] = true
-        count++
-        randomRotation === 1 ? X++ : Y++
-    }
-}
+        computer.b[Y][X] = shipsId[int].s;
+        computer.c[`${Y}${X}`] = true;
+        count++;
+        randomRotation === 1 ? X++ : Y++;
+    };
+};
 
 function randomCoor(int) {
-    let min = Math.ceil(0)
-    let max = Math.floor(11 - shipsId[int].l)
-    let randomNum = Math.floor(Math.random() * (max - min) + min)
-    return randomNum
-}
+    let min = Math.ceil(0);
+    let max = Math.floor(11 - shipsId[int].l);
+    let randomNum = Math.floor(Math.random() * (max - min) + min);
+    return randomNum;
+};
 
-function compFire() {
-    let min = Math.ceil(0)
-    let max = Math.floor(Math.random() * 10)
-}
+function randomFireCoor() {
+    let min = Math.ceil(0);
+    let max = Math.floor(Math.random() * 10);
+};
 
 
-function Compguard(){
-
-}
